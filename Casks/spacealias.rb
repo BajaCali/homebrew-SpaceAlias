487adbbf3c7bb7e94df58cddcb4e1381c349a611
Michal Němec <michaelnemec47@gmail.com>
2023-01-25 21:48:18 +0100

    Update cask; cleal folders

--- Casks/spacealias.rb
+++ Casks/spacealias.rb
@@ -1,11 +1,11 @@
 cask "spacealias" do
-  version "1.0.0"
-  sha256 "ab07d43d75ad9bf4d1bee38794cb19ecf5b79834f627c94298ecb2321cccacd0"
+  version "1.0.2"
+  sha256 "7a12ee340bbc62a38f651cd909691f0a998b7552bb6f61d6be84b6522ac761b5"
 
-  url "https://gitlab.com/BajaCali/spacealias/-/archive/v#{version}/spacealias-v#{version}.tar.gz"
+  url "https://gitlab.com/BajaCali/spacealias/-/raw/v1.0.2/build/SpaceAlias.dmg"
   name "SpaceAlias"
   desc "Show desktop number and alias them in your menu bar!"
   homepage "https://gitlab.com/BajaCali/spacealias"
 
-  app "build/SpaceAlias.app"
+  app "SpaceAlias.app"
 end
